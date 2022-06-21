1cba24394ff00f49b4b9900da5c57a83d355bd6a
Ineke Vermeulen <inekevermeulen@gmail.com>
2022-06-21 14:10:04 +0200

    improved styling of pagination

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -7,90 +7,79 @@ defineProps<{
 </script>
 
 <template>
-  <nav>
-    <ul>
-      <li>
-        <RouterLink
-          :to="{ name: routeName, query: { page: 1 } }"
-          :class="{ disabled: activePage === 1 }"
-        >
-          &#10094;&#10094;
-        </RouterLink>
-      </li>
-      <li>
-        <RouterLink
-          :to="{ name: routeName, query: { page: activePage - 1 } }"
-          :class="{ disabled: activePage === 1 }"
-        >
-          &#10094;
-        </RouterLink>
-      </li>
-      <li>
-        <span>{{ activePage }}/{{ pageCount }}</span>
-      </li>
-      <li :class="{ disabled: activePage === pageCount }">
-        <RouterLink
-          :to="{ name: routeName, query: { page: activePage + 1 } }"
-          :class="{ disabled: activePage === pageCount }"
-        >
-          &#10095;
-        </RouterLink>
-      </li>
-      <li :class="{ disabled: activePage === pageCount }">
-        <RouterLink
-          :to="{ name: routeName, query: { page: pageCount } }"
-          :class="{ disabled: activePage === pageCount }"
-        >
-          &#10095;&#10095;
-        </RouterLink>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <nav>
+      <RouterLink
+        :to="{ name: routeName, query: { page: 1 } }"
+        :class="{ disabled: activePage === 1 }"
+      >
+        &#10094;&#10094;
+      </RouterLink>
+      <RouterLink
+        :to="{ name: routeName, query: { page: activePage - 1 } }"
+        :class="{ disabled: activePage === 1 }"
+      >
+        &#10094;
+      </RouterLink>
+      <span>{{ activePage }}/{{ pageCount }}</span>
+      <RouterLink
+        :to="{ name: routeName, query: { page: activePage + 1 } }"
+        :class="{ disabled: activePage === pageCount }"
+      >
+        &#10095;
+      </RouterLink>
+      <RouterLink
+        :to="{ name: routeName, query: { page: pageCount } }"
+        :class="{ disabled: activePage === pageCount }"
+      >
+        &#10095;&#10095;
+      </RouterLink>
+    </nav>
+  </div>
 </template>
 
 <style scoped lang="scss">
-nav {
+div {
   display: flex;
+  margin: 15px 0;
   justify-content: center;
-  grid-column: 1 / span 8;
+  grid-column: 1 / span 6;
 
-  ul {
-    list-style: none;
-    margin: 20px 10px;
-
-    li {
+  nav {
+    a {
+      font-family: "Courier New", Courier, monospace;
+      font-size: 16px;
       display: inline-block;
+      width: 30px;
+      height: 30px;
+      margin: 0 5px;
+      border-radius: 5px;
+      border: 1px solid gray;
+      color: black;
+      text-decoration: none;
+      transition: background-color 0.3s;
+      text-align: center;
+      vertical-align: middle;
+      line-height: 32px;
 
-      a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 30px;
-        height: 30px;
-        border-radius: 5px;
-        border: 1px solid gray;
-        color: black;
-        margin: 5px;
-        text-decoration: none;
-
-        &:hover {
-          background-color: lightgray;
-        }
-
-        &.active {
-          background-color: orangered;
-        }
-
-        &.disabled {
-          pointer-events: none;
-          color: lightgray;
-          cursor: not-allowed;
-          border: 1px solid lightgray;
-        }
+      &:hover {
+        background-color: lightgray;
       }
 
-      span {
+      &.disabled {
+        pointer-events: none;
+        color: lightgray;
+        cursor: not-allowed;
+        border: 1px solid lightgray;
+      }
+    }
+
+    span {
+      display: none;
+
+      @media screen and (min-width: 740px) {
         margin: 0 5px;
+        display: inline-block;
       }
     }
   }
